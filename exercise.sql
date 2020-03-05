f0980e903a58a486da9cb1b20bd4a08b040fcda7
Candace <2100978@talnet.nl>
2020-03-05 15:45:36 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,18 +1,6 @@
 
--- TRUNCATE planeten;
-SELECT * FROM planeten;
 
-CREATE TABLE  planeten(
-	naam varchar(20),
-	diameter INT ,
-    afstand_tot_de_zon float,
-    massa float ,
-    bezoek_datum date NOT NULL,
-    id mediumint NOT NULL AUTO_INCREMENT primary key
-);
-
-insert into planeten values(
-	"Neptunus","49572","4498252900","17","10"
-
-);
 UPDATE `sterrenstelsel`.`planeten` SET `naam` = 'Teenalp' WHERE (`id` = '10');
+
+DELETE from planeten
+WHERE id = 10;
